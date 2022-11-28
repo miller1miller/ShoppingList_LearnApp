cdd939a4712f400f1a8faa71d97a09021987c891
Yuriy Lemaykin <y.lemaykin@gmail.com>
2022-11-28 18:38:35 +0300

    Created ViewModel

---
+++ app/src/main/java/com/gmiller/shoppinglist_learnapp/presentation/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.gmiller.shoppinglist_learnapp.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.gmiller.shoppinglist_learnapp.data.ShopListRepositoryImpl
+import com.gmiller.shoppinglist_learnapp.domain.DeleteShopItemUseCase
+import com.gmiller.shoppinglist_learnapp.domain.EditShopItemUseCase
+import com.gmiller.shoppinglist_learnapp.domain.GetShopListUseCase
+import com.gmiller.shoppinglist_learnapp.domain.ShopItem
+
+class MainViewModel: ViewModel() {
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
